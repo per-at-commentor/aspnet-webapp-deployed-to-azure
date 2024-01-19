f5f34919532253b5ae8aacb76f422856331ffa50
Per Fuglsang Møller <pfm@solita.dk>
2024-01-19 20:43:19 +0100

    test removing ARM secrets

--- terraform.tf
+++ terraform.tf
@@ -24,8 +24,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Create App Service Plan
-resource "azurerm_service_plan" "appserviceplan" {
-  name                = "${var.APP_NAME}-appserviceplan"
+resource "azurerm_service_plan" "serviceplan" {
+  name                = "${var.APP_NAME}-serviceplan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
@@ -37,7 +37,7 @@ resource "azurerm_linux_web_app" "webapp" {
   name                  = "${var.APP_NAME}-webapp"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  service_plan_id       = azurerm_service_plan.serviceplan.id
   https_only            = true
   site_config { 
     minimum_tls_version = "1.2"
