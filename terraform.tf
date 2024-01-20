a430537d59c6d4e778c0c8998e650b4436e9e4db
Per Fuglsang Møller <pfm@solita.dk>
2024-01-20 19:52:53 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -41,6 +41,9 @@ resource "azurerm_linux_web_app" "webapp" {
   https_only            = true
   site_config { 
     minimum_tls_version = "1.2"
+    application_stack {
+      dotnet_version    = "8.0"
+    }
   }
 }
 
