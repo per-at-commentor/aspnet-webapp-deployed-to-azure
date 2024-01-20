005b0d9b5f031c24b8175c76a450464c9323ec9e
Per Fuglsang Møller <pfm@solita.dk>
2024-01-20 11:32:00 +0100

    save environment variables and secrets

--- terraform.tf
+++ terraform.tf
@@ -24,7 +24,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Create App Service Plan
-resource "azurerm_service_plan" "serviceplan" {
+resource "azurerm_service_plan" "asp" {
   name                = "${var.APP_NAME}-${var.ENV_NAME}-asp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -37,9 +37,30 @@ resource "azurerm_linux_web_app" "webapp" {
   name                  = "${var.APP_NAME}-${var.ENV_NAME}-webapp"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  service_plan_id       = azurerm_service_plan.serviceplan.id
+  service_plan_id       = azurerm_service_plan.asp.id
   https_only            = true
   site_config { 
     minimum_tls_version = "1.2"
   }
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+output "resource_group_id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "appserviceplan_name" {
+  value = azurerm_service_plan.asp.name
+}
+output "appserviceplan_id" {
+  value = azurerm_service_plan.asp.id
+}
+
+output "webapp_name" {
+  value = azurerm_linux_web_app.webapp.name
+}
+output "webapp_id" {
+  value = azurerm_linux_web_app.webapp.id
 }
