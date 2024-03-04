f6a4be7d7b1124188b5f2e92f750069a1e5c250f
Per Fuglsang Møller <141002900+per-at-commentor@users.noreply.github.com>
2024-03-04 14:10:25 +0100

    xUnit project with WebApplicationFactory (#29)
    * xUnit project with WebApplicationFactory
    
    * add new TestProject2 to workflow
    
    * Make test parse
    
    * Test privacy page
    
    * Test error page
    
    ---------
    
    Co-authored-by: Per Fuglsang Møller <pfm@solita.dk>

---
+++ src/TestProject2/HomeControllerTest.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using WebApplication1.Controllers;
+
+namespace TestProject2;
+
+public class HomeControllerTest
+{
+    [Fact]
+    public async Task HomePage()
+    {
+        var factory = new WebApplicationFactory<HomeController>();
+        var client = factory.CreateDefaultClient();
+        var result = await client.GetAsync("/Home");
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        var body = await result.Content.ReadAsStringAsync();
+        Assert.Contains("Hello world!", body);
+    }
+
+    [Fact]
+    public async Task PrivacyPage()
+    {
+        var factory = new WebApplicationFactory<HomeController>();
+        var client = factory.CreateDefaultClient();
+        var result = await client.GetAsync("/Home/Privacy");
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        var body = await result.Content.ReadAsStringAsync();
+        Assert.Contains("Use this page to detail your site's privacy policy.", body);
+    }
+
+    [Fact]
+    public async Task ErrorPage()
+    {
+        var factory = new WebApplicationFactory<HomeController>();
+        var client = factory.CreateDefaultClient();
+        var result = await client.GetAsync("/Home/Error");
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        var body = await result.Content.ReadAsStringAsync();
+        Assert.Contains("An error occurred while processing your request.", body);
+    }
+}
