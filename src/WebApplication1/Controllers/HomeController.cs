d72a2fa40a506288dc13c305ebc47bb6932f8aa9
Per Fuglsang Møller <pfm@solita.dk>
2024-03-04 13:09:28 +0100

    fix code smells

--- src/WebApplication1/Controllers/HomeController.cs
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -5,13 +5,6 @@ using WebApplication1.Models;
 namespace WebApplication1.Controllers;
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
